24f87f61928fcfb458904de4f0a3f44496ddcb5d
Your Name <adrien.ardra@ui.ac.id>
2023-08-28 21:33:53 +0700

    revise

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './page/profile/profile.js';
 import AboutMe from './page/aboutMe/aboutMe.js';
 import Projects from './page/projects/projects.js';
-import Test from './page/test/test.js';
 import Navibar from './components/navibar/navibar.js';
 import Footer from './components/footer/footer.js';
 
